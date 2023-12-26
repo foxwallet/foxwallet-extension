299c84d1ecec6767a3bb60c27a92bf2a85fecf61
LouisWT <liuwentong@web3.com>
2023-12-26 12:46:47 +0800

    feat: home page & locales (#3)

--- app/App.tsx
+++ app/App.tsx
@@ -2,9 +2,20 @@ import { useRoutes } from "react-router-dom";
 import { routesConfig } from "./routes";
 import ErrorBoundary from "./components/Custom/ErrorBoundary";
 import { ViewPort } from "./components/Custom/ViewPort";
+import { store } from "./store/store";
+import { useEffect } from "react";
+import { changeLanguage } from "./locales/i18";
 
 function App() {
   const routes = useRoutes(routesConfig);
+
+  useEffect(() => {
+    const language = store.getState().setting.language;
+    if (language) {
+      changeLanguage(language);
+    }
+  }, []);
+
   return (
     <ViewPort>
       <ErrorBoundary>{routes}</ErrorBoundary>
