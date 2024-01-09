04b487f6cbb1b8607251a6db4802391046582f65
LouisWT <liuwentong@web3.com>
2024-01-09 15:19:05 +0800

    feat: add reset status

---
+++ app/components/Setting/ConfirmResyncDialog/index.tsx
@@ -0,0 +1,39 @@
+import { Button, Flex } from "@chakra-ui/react";
+import { P3 } from "../../../common/theme/components/text";
+import { BasicModal } from "../../Custom/Modal";
+import { promisifyChooseDialogWrapper } from "../../../common/utils/dialog";
+import { useTranslation } from "react-i18next";
+
+interface Props {
+  isOpen: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmResyncDialog = (props: Props) => {
+  const { isOpen, onConfirm, onCancel } = props;
+  const { t } = useTranslation();
+
+  return (
+    <BasicModal
+      isOpen={isOpen}
+      onClose={onCancel}
+      isCentered
+      title={t("Common:warning")}
+      body={<P3 textAlign={"center"}>{t("Resync:remind")}</P3>}
+      footer={
+        <Flex flex={1}>
+          <Button flex={1} mr="2" onClick={onConfirm}>
+            {t("Common:confirm")}
+          </Button>
+          <Button flex={1} ml="2" colorScheme="secondary" onClick={onCancel}>
+            {t("Common:cancel")}
+          </Button>
+        </Flex>
+      }
+    />
+  );
+};
+
+export const showConfirmResyncDialog =
+  promisifyChooseDialogWrapper(ConfirmResyncDialog);
--- app/pages/Me/Settings/index.tsx
+++ app/pages/Me/Settings/index.tsx
@@ -4,12 +4,16 @@ import {
   IconInfo,
   IconLanguage,
 } from "@/components/Custom/Icon";
+import { showConfirmResyncDialog } from "@/components/Setting/ConfirmResyncDialog";
 import SettingItem from "@/components/Setting/SettingItem";
+import { useClient } from "@/hooks/useClient";
+import { useCurrAccount } from "@/hooks/useCurrAccount";
 import { usePopupSelector } from "@/hooks/useStore";
 import { Content } from "@/layouts/Content";
 import { PageWithHeader } from "@/layouts/Page";
 import { LanguageLabels } from "@/locales/i18";
 import { CURRENCY } from "core/constants";
+import { CoinType } from "core/types";
 import { isEqual } from "lodash";
 import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
@@ -25,6 +29,8 @@ const SettingsScreen = () => {
     }),
     isEqual,
   );
+  const { selectedAccount } = useCurrAccount();
+  const { popupServerClient } = useClient();
 
   const onModifyPassword = useCallback(() => {
     navigate("/change_password");
@@ -37,6 +43,17 @@ const SettingsScreen = () => {
   const onCurrency = useCallback(() => {
     navigate("/manage_currency");
   }, [navigate]);
+
+  const onResetAleoStatus = useCallback(async () => {
+    try {
+      const { confirmed } = await showConfirmResyncDialog();
+      if (confirmed) {
+        return await popupServerClient.rescanAleo();
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, [popupServerClient]);
 
   const onAbout = useCallback(() => {
     navigate("/about");
@@ -62,6 +79,14 @@ const SettingsScreen = () => {
           icon={<IconCurrency w={4} h={4} />}
           onPress={onCurrency}
         /> */}
+        {selectedAccount.coinType === CoinType.ALEO && (
+          <SettingItem
+            title={t("Reset current account's status")}
+            icon={<IconCurrency w={4} h={4} />}
+            noNext
+            onPress={onResetAleoStatus}
+          />
+        )}
         <SettingItem
           title={t("About:title")}
           icon={<IconInfo w={4} h={4} />}
