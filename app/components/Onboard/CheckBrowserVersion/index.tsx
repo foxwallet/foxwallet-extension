b51a25c9e87b4a32e9ada11dfa524b40a77331c9
ljunb <cookiejlim@gmail.com>
2024-01-13 11:36:58 +0800

    dev: optimize check version logic

---
+++ app/components/Onboard/CheckBrowserVersion/index.tsx
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+import UpgradeReminderScreen from "@/pages/Onboard/UpgradeReminder";
+import { CHROME_MIN_VERSION } from "@/common/constants";
+
+export const CheckBrowserVersion = (props: { children: React.ReactNode }) => {
+  const showReminder = useMemo(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (!userAgent.includes("chrome")) {
+      return false;
+    }
+
+    const matched = userAgent.match(/chrome\/[\d.]+/gi);
+    if (matched?.[0]) {
+      const versionCmps = matched[0].split("/");
+      return parseFloat(versionCmps[1]) >= parseFloat(CHROME_MIN_VERSION);
+    }
+
+    return false;
+  }, []);
+
+  if (showReminder) {
+    return <UpgradeReminderScreen />;
+  }
+
+  return <>{props.children}</>;
+};
