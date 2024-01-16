2032a56df2c9355f0261f0fa54a1e51c619735d7
LouisWT <liuwentong@web3.com>
2024-01-16 19:25:03 +0800

    dev: use .env to store environment variables (#63)

---
+++ env/index.ts
@@ -0,0 +1,15 @@
+import { shuffle } from "@/common/utils/array";
+
+export const ReserveChainConfigs = {
+  ALEO_TESTNET3: {
+    rpcList: shuffle(
+      JSON.parse(import.meta.env.VITE_ALEO_RPC_LIST) as string[],
+    ),
+    syncApiList: shuffle(
+      JSON.parse(import.meta.env.VITE_ALEO_SYNC_API_LIST) as string[],
+    ),
+    walletApiList: shuffle(
+      JSON.parse(import.meta.env.VITE_ALEO_WALLET_API_LIST) as string[],
+    ),
+  },
+};
