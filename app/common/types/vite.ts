28018afb0713fad27375258e7d1d05bde4c739d9
LouisWT <liuwentong@web3.com>
2024-01-16 17:49:49 +0800

    dev: use .env to store environment variables

---
+++ app/common/types/vite.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ALEO_RPC_LIST: string;
+  readonly VITE_ALEO_SYNC_API_LIST: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
