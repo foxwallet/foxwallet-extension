ddf7603f4add03a4717106d2116c5e5de06748df
LouisWT <liuwentong@web3.com>
2024-01-31 21:00:10 +0800

    dev: revert the modal root (#70)

--- app/index.tsx
+++ app/index.tsx
@@ -13,6 +13,8 @@ import { ChakraBaseProvider } from "@chakra-ui/react";
 import { theme } from "./common/theme";
 import { PersistGate } from "redux-persist/integration/react";
 import { getPersistor } from "@rematch/persist";
+import { GlobalModal } from "./common/utils/dialog";
+import { ViewPort } from "./components/Custom/ViewPort";
 
 const persistor = getPersistor();
 
@@ -31,5 +33,20 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         </ClientContext.Provider>
       </ChakraBaseProvider>
     </HashRouter>
+  </StrictMode>,
+);
+
+// pure modal, can't access other state in the modal, provide state by props
+ReactDOM.createRoot(
+  document.getElementById("modal-root") as HTMLElement,
+).render(
+  <StrictMode>
+    <ChakraBaseProvider theme={theme}>
+      <Provider store={store}>
+        <ViewPort>
+          <GlobalModal />
+        </ViewPort>
+      </Provider>
+    </ChakraBaseProvider>
   </StrictMode>,
 );
