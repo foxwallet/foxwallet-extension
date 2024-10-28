c8777fb692103d6c9648506fb8704f1b98e06ace
monster1935 <monster.1935.li@gmail.com>
2024-10-28 15:59:33 +0800

    feat: add stake guide dialog

---
+++ app/components/Custom/DownloadDialog/index.tsx
@@ -0,0 +1,33 @@
+import { BasicModal } from '@/components/Custom/Modal'
+import { promisifyChooseDialogWrapper } from "../../../common/utils/dialog";
+import { useTranslation } from "react-i18next";
+import { Image, Center, Text } from "@chakra-ui/react";
+import Qrcode from '@/common/assets/image/qrcode.png';
+
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void
+}
+
+const downloadDialog = (props: Props) => {
+  const { isOpen, onClose } = props;
+  const { t } = useTranslation();
+
+  const body = () => <div>
+    <Text textAlign="center">
+      {t("Stake:dialogContent")}
+    </Text>
+    <Center>
+      <Image src={Qrcode} alt="qrcode" boxSize='170px' />
+    </Center>
+  </div>
+
+  return (
+    <BasicModal
+      isOpen={isOpen} title={t("Stake:dialogTitle")} body={body()} onClose={onClose} />
+  );
+};
+
+export const showDownloadDialog =
+  promisifyChooseDialogWrapper(downloadDialog);
