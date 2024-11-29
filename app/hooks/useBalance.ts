2f32b72e855b682fe667229ac15770c0aa952782
miaomiao <miaomiao@foxwallet.com>
2024-11-29 15:04:00 +0800

    useBalance

---
+++ app/hooks/useBalance.ts
@@ -0,0 +1,36 @@
+import { type ChainUniqueId } from "core/types/ChainUniqueId";
+import { type TokenV2 } from "core/types/Token";
+import { useCoinService } from "@/hooks/useCoinService";
+import { useEffect, useState } from "react";
+
+export const useBalance = (
+  uniqueId: ChainUniqueId,
+  address: string,
+  token?: TokenV2,
+) => {
+  const [balance, setBalance] = useState<bigint>(0n);
+  const [loadingBalance, setLoadingBalance] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const { coinService } = useCoinService(uniqueId);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        setLoadingBalance(true);
+        const balanceResult = await coinService.getBalance(address);
+        setBalance(balanceResult.total);
+      } catch (err) {
+        setError(
+          err instanceof Error ? err : new Error("Failed to fetch balance"),
+        );
+      } finally {
+        setLoadingBalance(false);
+      }
+    };
+
+    fetchBalance();
+  }, [uniqueId, address, coinService]);
+
+  return { balance, loadingBalance, error };
+};
