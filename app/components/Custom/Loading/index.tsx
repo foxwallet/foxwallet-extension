bb38da21153cfd5527d857a5c1f7a6d15c3c9fdd
miaomiao <miaomiao@foxwallet.com>
2024-11-29 11:27:03 +0800

    add LoadingView

--- app/components/Custom/Loading/index.tsx
+++ app/components/Custom/Loading/index.tsx
@@ -1,5 +1,7 @@
 import { H1 } from "@/common/theme/components/text";
-import { Flex, Spinner } from "@chakra-ui/react";
+import { Flex, keyframes, Spinner } from "@chakra-ui/react";
+import { IconLoading } from "@/components/Custom/Icon";
+import type React from "react";
 
 interface LoadingProps {}
 
@@ -8,5 +10,22 @@ export const LoadingScreen = (props: LoadingProps) => {
     <Flex w="full" h="full" justifyContent={"center"} alignItems={"center"}>
       <Spinner w={10} h={10} />
     </Flex>
+  );
+};
+
+export const LoadingView = () => {
+  const rotateAnimation = keyframes`
+  from { transform: rotate(0deg) }
+  to { transform: rotate(360deg) }
+`;
+
+  return (
+    <IconLoading
+      w={5}
+      h={5}
+      mr={1}
+      stroke={"green.600"}
+      animation={`${rotateAnimation} infinite 2s linear`}
+    />
   );
 };
