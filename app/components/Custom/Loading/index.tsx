aeb3c36060fe286a056bbd4110807f01995c5e82
miaomiao <miaomiao@foxwallet.com>
2024-12-02 11:43:02 +0800

    add LoadingOverlay

--- app/components/Custom/Loading/index.tsx
+++ app/components/Custom/Loading/index.tsx
@@ -1,9 +1,12 @@
 import { H1 } from "@/common/theme/components/text";
-import { Flex, keyframes, Spinner } from "@chakra-ui/react";
+import { Box, Flex, keyframes, Spinner } from "@chakra-ui/react";
 import { IconLoading } from "@/components/Custom/Icon";
 import type React from "react";
 
-interface LoadingProps {}
+interface LoadingProps {
+  isLoading?: boolean;
+  children?: React.ReactNode;
+}
 
 export const LoadingScreen = (props: LoadingProps) => {
   return (
@@ -27,5 +30,31 @@ export const LoadingView = () => {
       stroke={"green.600"}
       animation={`${rotateAnimation} infinite 2s linear`}
     />
+  );
+};
+
+export const LoadingOverlay = (props: LoadingProps) => {
+  const { isLoading = true, children } = props;
+  return (
+    <Box position="relative">
+      {isLoading && (
+        <Box
+          position="fixed"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          backgroundColor="rgba(0, 0, 0, 0.3)"
+          zIndex="9999"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {/* <Spinner size="xl" color="white" thickness="4px" /> */}
+          <Spinner w={10} h={10} />
+        </Box>
+      )}
+      {children}
+    </Box>
   );
 };
--- app/pages/Wallet/Send/index.tsx
+++ app/pages/Wallet/Send/index.tsx
@@ -110,6 +110,7 @@ const SendScreen = () => {
       }}
     >
       {sendTokenContent}
+      <LoadingOverlay isLoading={isSending} />
     </PageWithHeader>
   );
 };
