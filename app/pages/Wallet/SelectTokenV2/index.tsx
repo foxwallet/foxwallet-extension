695b9cbae6b16e507da4f1f8d20c47029dcaacaf
miaomiao <miaomiao@foxwallet.com>
2025-01-06 13:33:19 +0800

    optimize send receive screens

--- app/pages/Wallet/SelectTokenV2/index.tsx
+++ app/pages/Wallet/SelectTokenV2/index.tsx
@@ -1,52 +1,74 @@
 import {
   Box,
-  Flex,
-  Image,
   Input,
   InputGroup,
   InputLeftElement,
-  Text,
   VStack,
 } from "@chakra-ui/react";
 import type React from "react";
 import { useCallback, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { IconSearch, IconTokenPlaceHolder } from "@/components/Custom/Icon";
+import { IconSearch } from "@/components/Custom/Icon";
 import { useDebounce } from "use-debounce";
 import {
   type ChainUniqueId,
   InnerChainUniqueId,
 } from "core/types/ChainUniqueId";
-import { Content } from "@/layouts/Content";
 import { PageWithHeader } from "@/layouts/Page";
 import { useNavigate, useParams } from "react-router-dom";
 import { NextAction } from "@/pages/Wallet/SelectNetwork";
 import { type TokenV2 } from "core/types/Token";
-import { useAssetList } from "@/hooks/useAssetList";
 import { useGroupAccount } from "@/hooks/useGroupAccount";
 import { serializeToken } from "@/common/utils/string";
 import { TokenItemWithBalance } from "@/components/Wallet/TokenItem";
+import { usePopupSelector } from "@/hooks/useStore";
+import { isEqual } from "lodash";
+import { useSafeParams } from "@/hooks/useSafeParams";
+import { useAllTokens, useRecommendTokens } from "@/hooks/useToken";
 
 const SelectTokenScreenV2 = () => {
-  const {
-    uniqueId = InnerChainUniqueId.ETHEREUM,
-    action = NextAction.Receive,
-  } = useParams<{
+  const { action = NextAction.Receive } = useParams<{
     uniqueId: ChainUniqueId;
     action: NextAction;
   }>();
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const { uniqueId } = useSafeParams();
 
   const [searchStr, setSearchStr] = useState("");
   const [debounceSearchStr] = useDebounce(searchStr, 500);
 
-  const { groupAccount, getMatchAccountsWithUniqueId } = useGroupAccount();
+  const { getMatchAccountsWithUniqueId } = useGroupAccount();
 
   const selectedAccount = useMemo(() => {
     return getMatchAccountsWithUniqueId(uniqueId)[0];
   }, [getMatchAccountsWithUniqueId, uniqueId]);
-  const { assets } = useAssetList(uniqueId, selectedAccount.account.address);
+
+  const recommendTokens = useRecommendTokens(uniqueId); // 推荐 数量适中
+  // console.log("      recommendTokens", recommendTokens);
+  //
+  // const rawAllTokens = useAllTokens(uniqueId); // 所有 数量很多
+  // console.log("      rawAllTokens", rawAllTokens);
+  //
+  // const whiteListTokens = useAllWhiteTokens(uniqueId); // 白名单 数量较多
+  // console.log("      whiteListTokens", whiteListTokens);
+  //
+  // const { assets: addressTokens } = useGroupAccountAssets();
+  // console.log("      addressTokens", addressTokens);
+
+  const userTokens = usePopupSelector(
+    (state) =>
+      state.tokens.userTokens?.[uniqueId]?.[selectedAccount?.account.address],
+    isEqual,
+  );
+
+  const assets = useMemo(() => {
+    if (action === NextAction.Send) {
+      return userTokens ?? [];
+    } else {
+      return recommendTokens;
+    }
+  }, [action, recommendTokens, userTokens]);
 
   const onKeywordChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -114,7 +136,7 @@ const SelectTokenScreenV2 = () => {
           bg={"gray.50"}
           value={searchStr}
           onChange={onKeywordChange}
-          placeholder={t("Networks:searchHint")}
+          placeholder={t("SelectToken:searchHint")}
           pl={10}
           py={2}
         />
