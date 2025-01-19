59228519bcb620215afc2de767eea910e0c5fb19
miaomiao <miaomiao@foxwallet.com>
2025-01-19 09:18:46 +0800

    add useUserAssetsWithPrice

--- app/hooks/useGroupAccountAssets.ts
+++ app/hooks/useGroupAccountAssets.ts
@@ -3,19 +3,20 @@ import { useChainMode } from "@/hooks/useChainMode";
 import { InnerChainUniqueId } from "core/types/ChainUniqueId";
 import { usePopupDispatch, usePopupSelector } from "@/hooks/useStore";
 import { isEqual } from "lodash";
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import { useGroupInteractiveTokens } from "@/hooks/useToken";
 import { coinServiceEntry } from "core/coins/CoinServiceEntry";
 import { AssetType, type TokenV2 } from "core/types/Token";
 import { ALEO_NATIVE_TOKEN } from "core/coins/ALEO/config/chains";
-import { useSelector } from "react-redux";
 import { coinPriceSelector } from "@/store/selectors/token";
 import { type ExchangeItem } from "@/store/coinPriceModelV2";
 import { useMultiChainPrice } from "@/hooks/useTokenPrice";
+import type { BalanceResp } from "core/types/Balance";
+import useSWR from "swr";
 
 export const useUserAssets = () => {
   const { getMatchAccountsWithUniqueId } = useGroupAccount();
-  const { availableChains, availableChainUniqueIds } = useChainMode();
+  const { availableChains } = useChainMode();
 
   const assetIdentifiers = availableChains.map((item) => {
     const selectAccount = getMatchAccountsWithUniqueId(item.uniqueId)[0];
@@ -60,6 +61,12 @@ export const useUserAssets = () => {
     return res.flat();
   }, [userTokensMap]);
 
+  return assets;
+};
+
+export const useUserAssetsWithPrice = () => {
+  const assets = useUserAssets();
+  const { availableChainUniqueIds } = useChainMode();
   const { getMultiChainPrice } = useMultiChainPrice(availableChainUniqueIds);
 
   useEffect(() => {
@@ -93,6 +100,88 @@ export const useUserAssets = () => {
   return assetsWithPrice;
 };
 
+export const useUserAssetsWithPriceBalance = () => {
+  const assets = useUserAssetsWithPrice();
+
+  const fetchData = useCallback(async () => {
+    console.log("      fetchData useUserAssetsWithPriceBalance");
+    const promises = assets.map(async (item) => {
+      const {
+        uniqueId,
+        contractAddress,
+        programId,
+        tokenId,
+        ownerAddress: address,
+        type,
+      } = item;
+      const coinService = coinServiceEntry.getInstance(uniqueId);
+
+      let balance: BalanceResp | undefined;
+      if (type === AssetType.TOKEN) {
+        if (contractAddress) {
+          balance = await coinService.getTokenBalance({
+            address,
+            token: { contractAddress },
+          });
+        } else {
+          if (
+            uniqueId === InnerChainUniqueId.ALEO_MAINNET &&
+            programId &&
+            tokenId
+          ) {
+            balance = await coinService.getTokenBalance({
+              address,
+              token: { contractAddress: `${programId}-${tokenId}` },
+            });
+          }
+        }
+      } else {
+        balance = await coinService.getBalance(address);
+      }
+      return balance ? { ...item, ...balance } : item;
+    });
+    const results = await Promise.allSettled(promises);
+
+    const validResults: TokenV2[] = results.map((result, index) => {
+      if (result.status === "fulfilled") {
+        return result.value;
+      } else {
+        return assets[index];
+      }
+    });
+    // console.log("      validResults", validResults);
+    return validResults;
+  }, [assets]);
+
+  const key = useMemo(() => {
+    const sortedAssets = [...assets].sort(
+      (a, b) =>
+        a.uniqueId.localeCompare(b.uniqueId) ||
+        a.contractAddress.localeCompare(b.contractAddress),
+    );
+    const temp = sortedAssets
+      .map((asset) => `${asset.uniqueId}-${asset.contractAddress}`)
+      .join("-");
+    return `/useUserAssetsWithPriceBalance/${temp}`;
+  }, [assets]);
+
+  const {
+    data: userAssetsWithPriceBalance,
+    error,
+    mutate: getUserAssetsWithPriceBalance,
+    isLoading: loadingUserAssetsWithPriceBalance,
+  } = useSWR(key, fetchData, {
+    refreshInterval: 5 * 1000,
+  });
+
+  return {
+    userAssetsWithPriceBalance,
+    error,
+    getUserAssetsWithPriceBalance,
+    loadingUserAssetsWithPriceBalance,
+  };
+};
+
 export const useGroupAccountAssets = () => {
   const { getMatchAccountsWithUniqueId } = useGroupAccount();
   const { availableChains } = useChainMode();
@@ -107,7 +196,6 @@ export const useGroupAccountAssets = () => {
   const needUpdateMap = usePopupSelector((state) => {
     const res = assetIdentifiers.map((item) => {
       const { address, uniqueId } = item;
-      const now = Date.now();
       const lastUpdateTimestamp =
         state.tokens?.lastUpdateTimestamp[uniqueId]?.[address];
       const needUpdate = !lastUpdateTimestamp;
@@ -124,20 +212,6 @@ export const useGroupAccountAssets = () => {
     needUpdateMap,
     true,
   );
-
-  // 获取用户的代币数据
-  // const userTokensMap = usePopupSelector((state) => {
-  //   const res = assetIdentifiers.map((item) => {
-  //     const { address, uniqueId } = item;
-  //     const tokens = state.tokens.userTokens[uniqueId]?.[address] ?? [];
-  //     return {
-  //       address,
-  //       uniqueId,
-  //       tokens,
-  //     };
-  //   });
-  //   return res;
-  // }, isEqual);
 
   useEffect(() => {
     const updateTokens = async () => {
@@ -164,32 +238,8 @@ export const useGroupAccountAssets = () => {
     void updateTokens();
   }, [dispatch.tokens, getGroupInteractiveTokens]);
 
-  // const assets = useMemo(() => {
-  //   const res = userTokensMap.map((item) => {
-  //     const { uniqueId, address, tokens } = item;
-  //     const coinService = coinServiceEntry.getInstance(uniqueId);
-  //     const chainConfig = { ...coinService.config };
-  //     const nativeCurrency = { ...chainConfig.nativeCurrency };
-  //
-  //     const coin: TokenV2 = {
-  //       contractAddress: "",
-  //       decimals: nativeCurrency.decimals,
-  //       ownerAddress: address,
-  //       symbol: nativeCurrency.symbol,
-  //       type: AssetType.COIN,
-  //       uniqueId,
-  //       icon: chainConfig.logo,
-  //     };
-  //     const nativeToken =
-  //       uniqueId === InnerChainUniqueId.ALEO_MAINNET
-  //         ? { ...ALEO_NATIVE_TOKEN, ...coin }
-  //         : coin;
-  //     return [nativeToken, ...tokens];
-  //   });
-  //   return res.flat();
-  // }, [userTokensMap]);
-
-  const assets = useUserAssets();
+  const { userAssetsWithPriceBalance: assets } =
+    useUserAssetsWithPriceBalance();
 
   return { assets };
 };
